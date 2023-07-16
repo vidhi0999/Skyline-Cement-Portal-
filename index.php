6710fb98da5c2db968002e0214dac5850961e809
Vidhi <vidhithummar09@gmail.com>
2023-07-16 14:57:37 +0530

    first

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+
+<body>
+
+    <!-- header -->
+    <header class="header">
+        <div class="logo">
+            <img src="images/E-logo.png" alt="">
+            <div class="line"></div>
+            <p>CEMENT</p>
+        </div>
+
+        <div class="logo-space">
+        </div>
+    </header>
+
+    <!-- main section -->
+    <!-- <div class="main-container">
+    </div> -->
+
+    <!-- welcome -->
+    <div class="sub-container2">
+        <h1>Welcome To OUR WEBSITE</h1>
+    </div>
+
+    <!-- about us -->
+    <div class="aboutUs">
+        <div class="heading">
+
+            <div class="info-details">
+                <div class="info-sub-details">
+                    <h1>ABOUT US</h1>
+                    <a href="php/signup.php">
+                        <button>Explore more</button>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+    <!-- footer -->
+    <footer>
+        <p>Copyright © OnlineEdu- 2023</p>
+        <p>Contact us: onlineedu12@gmail.com </p>
+    </footer>
+    hello
+</body>
+
+</html>
---
+++ php/login.php
@@ -0,0 +1,9 @@
+<div class="form login">
+    <header>Login</header>
+    <form action="#">
+        <input type="text" placeholder="Email address" required />
+        <input type="password" placeholder="Password" required />
+        <a href="#">Forgot password?</a>
+        <input type="submit" value="Login" />
+    </form>
+</div>
