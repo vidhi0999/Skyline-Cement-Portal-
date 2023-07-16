2f38e8f02eb865cc6b8b30bb59f343d607e2cb66
Vidhi <vidhithummar09@gmail.com>
2023-07-16 15:39:49 +0530

    index

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/index.css">
-    hello
+
 </head>
 
 <body>
-    abc
+
     <!-- header -->
     <header class="header">
         <div class="logo">
